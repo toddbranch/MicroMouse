51ff3bb785cf84a19db6b452982f389c6523a8ea
Todd Branchflower <todd.branchflower@gmail.com>
2014-02-24 18:21:08 -0700

    added header to motor.h

--- motor.h
+++ motor.h
@@ -1,3 +1,21 @@
+/*
+ * motor.h
+ *
+ * Simple library to drive DC motors using the msp430g2553.
+ *
+ * The positive terminal of the right motor should be connected to P1.2.  The
+ * negative terminal of the right motor should be connected to P1.1.
+ *
+ * The positive terminal of the left motor should be connected to P2.1.  The
+ * negative terminal of the left motor should be connected to P2.0.
+ *
+ * This code can move the motors forward at any speed (currently configured for
+ * 60% duty cycle), but can only move them backward  at 50% speed.  This is due
+ * to using TA0.0, which is limited by hardware to 50%.  This limitation could
+ * be overcome by rewriting the code to generate PWM using GPIO and timers.
+ */
+
+
 // Individual Motor Commands
 void initLeftMotor();
 void moveLeftMotorForward();
