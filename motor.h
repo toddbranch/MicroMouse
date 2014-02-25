de175b58ad0cd2127970f53e6fc3f78dbc732c63
Todd Branchflower <todd.branchflower@gmail.com>
2014-02-24 18:11:13 -0700

    added simple motor code

---
+++ motor.h
@@ -0,0 +1,15 @@
+// Individual Motor Commands
+void initLeftMotor();
+void moveLeftMotorForward();
+void moveLeftMotorBackward();
+
+void initRightMotor();
+void moveRightMotorForward();
+void moveRightMotorBackward();
+
+// Combination Motor Commands
+void initMotors();
+void moveForward();
+void moveBackward();
+void turnLeft();
+void turnRight();
